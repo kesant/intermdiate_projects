f0d6f6d2b415e653708d869351fae89e0f265e0c
kevin santana <kevin_santana120@hotmail.com>
2022-07-09 20:29:11 -0500

    i crated a folder where i will be creating my snake game

--- turtle_race/main.py
+++ turtle_race/main.py
@@ -3,19 +3,36 @@
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from turtle import Turtle, Screen
+import random
 
-
-
+is_race_on=False
 screen=Screen()
 screen.setup(width=500,height=400)#we are defining the size of the screen
 user=screen.textinput(title="turtle race",prompt="wich turtle will win the race ? choose a color ")
 colors=["red","blue","yellow","orange","purple","green"]
 y_position=[-78,-50,-30,-10,20,40]
+all_turtles=[]
 for indice in range (0,6):
-    tim=Turtle(shape="turtle")
-    tim.color(colors[indice])
-    tim.penup()
-    tim.goto(x=-230,y=y_position[indice])
+    new_turtle=Turtle(shape="turtle")
+    new_turtle.color(colors[indice])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_position[indice])
+    all_turtles.append(new_turtle)
+if user:
+    is_race_on=True
+
+while is_race_on:
+    for turtle in all_turtles:
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+        if turtle.xcor()>=230:
+            print(f"the {turtle.pencolor()} turtle  is the winner ")
+            if turtle.pencolor()==user:
+                print("you win")
+            else :
+                print("you lose")
+            is_race_on=False
+
 
 screen.exitonclick()
 
