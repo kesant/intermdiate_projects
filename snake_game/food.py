7b3a499a3347d84a079b87a7e40eb6ff8afd8801
kevin santana <kevin_santana120@hotmail.com>
2022-07-12 22:36:14 -0500

    i complete the food class to respawn with every collision of the snake head with the food

--- snake_game/food.py
+++ snake_game/food.py
@@ -1,8 +1,15 @@
 from turtle import Turtle
-
-class Food(Turtle) :
+import  random
+class Food(Turtle) :#we make the food class being the child class of turtle
     def __init__(self):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize()
+        self.shapesize(stretch_len=0.5,stretch_wid=0.5)#we reduce half of the height and width of the circle
+        self.color("blue")
+        self.speed("fastest")
+        self.refres()
+    def refres(self):
+        random_x=random.randint(-280,280)
+        random_y =random.randint(-280, 280)
+        self.goto(random_x,random_y)
--- snake_game/main.py
+++ snake_game/main.py
@@ -1,12 +1,15 @@
-from turtle import  Turtle,Screen
+from turtle import  Screen
 import  time
+from food import  Food
 from snake import  Snake
 screen=Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")#we set the background color of the screen
 screen.title("My snake game")#we set the title of the screen
 screen.tracer(0)#puting 0 inside the tracer methods we turn off the animation
+
 snake=Snake()
+food=Food()#creamos la comida
 screen.listen()
 screen.onkey(fun=snake.up , key="Up")
 screen.onkey(fun=snake.down,key="Down")
@@ -17,8 +20,9 @@ while game_is_on:
     screen.update()  # update the animation and turn it on
     time.sleep(0.1)#it makes the time goes slower stop the time 0.1 seconds
     snake.move()
-
-
+    #detect the collision with the food
+    if snake.segments[0].distance(food)<15:#we use the distance method to measure the distance of the head of the snake with the food
+        food.refres()
 
 
 screen.exitonclick()
