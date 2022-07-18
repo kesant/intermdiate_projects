7bb0c609d5b88190fac2ba1b1f22ae8e88ac4f44
kevin santana <kevin_santana120@hotmail.com>
2022-07-17 22:43:44 -0500

    i created the ball class and the paddle class

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,17 +1,33 @@
 from turtle import Screen
 from paddle import  Paddle
+from ball import Ball
+
 import  time
+
 screen=Screen()
 screen.bgcolor("black")
 screen.setup(height=600,width=800)
 screen.title("Pong")
 screen.tracer(0)
 game_is_on=True
-padle=Paddle()
+
+padle=Paddle(350,0)
+padle2=Paddle(-350,0)
+
+ball=Ball()
+
+screen.listen()
+
+screen.onkey(fun=padle.up,key="Up")
+screen.onkey(fun=padle.down,key="Down")
+screen.onkey(fun=padle2.up,key="w")
+screen.onkey(fun=padle2.down,key="s")
+
 while game_is_on:
-    time.sleep(0.2)
+    time.sleep(0.1)
     screen.update()
-    padle.move()
+    ball.move()
+
 screen.exitonclick()
 
 
--- pong_game/paddle.py
+++ pong_game/paddle.py
@@ -2,13 +2,17 @@ from turtle import Turtle
 UP=90
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self,x_pos,y_pos):
         super().__init__()
         self.color("white")
         self.penup()
         self.shape("square")
         self.shapesize(stretch_len=1,stretch_wid=5)
-        self.goto(x=350,y=0)
+        self.goto(x=x_pos,y=y_pos)
 
-    def move(self):
-        self.forward(20)
+    def up(self):
+        new_y=self.ycor()+20
+        self.goto(self.xcor(),new_y)
+    def down(self):
+        new_y=self.ycor()-20
+        self.goto(self.xcor(),new_y)
