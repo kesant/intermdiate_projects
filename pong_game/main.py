4b1ec345d18050dc33e422f378669d9548a02b8a
kevin santana <kevin_santana120@hotmail.com>
2022-07-20 20:41:03 -0500

    I finished the pong game

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,8 +1,8 @@
 from turtle import Screen
-from paddle import  Paddle
+from paddle import Paddle
 from ball import Ball
-
-import  time
+from scoreboard import  Scoreboard
+import time
 
 screen=Screen()
 screen.bgcolor("black")
@@ -11,23 +11,37 @@ screen.title("Pong")
 screen.tracer(0)
 game_is_on=True
 
-padle=Paddle(350,0)
-padle2=Paddle(-350,0)
+score=Scoreboard()
+
+padle_r=Paddle(350,0)
+padle_l=Paddle(-350,0)
 
 ball=Ball()
 
 screen.listen()
 
-screen.onkey(fun=padle.up,key="Up")
-screen.onkey(fun=padle.down,key="Down")
-screen.onkey(fun=padle2.up,key="w")
-screen.onkey(fun=padle2.down,key="s")
+screen.onkey(fun=padle_r.up,key="Up")
+screen.onkey(fun=padle_r.down,key="Down")
+screen.onkey(fun=padle_l.up,key="w")
+screen.onkey(fun=padle_l.down,key="s")
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
-
+    #detect the colission with the wall
+    if ball.ycor()>285 or ball.ycor()<-285:
+        ball.bounce_y()
+    #detect the collision with the paddel
+    if ball.distance(padle_r)<50and ball.xcor()>320 or ball.distance(padle_l)<50and ball.xcor()<-320:
+        ball.bounce_x()
+    #detect if right paddle miss the ball
+    if ball.xcor()>380:
+        ball.restart()
+        score.l_point()
+    elif ball.xcor()<-380:
+        ball.restart()
+        score.r_point()
 screen.exitonclick()
 
 
