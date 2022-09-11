c8a9b9d8274bf6ad0d756384c9d85c12d4e349af
kevin santana <kevin_santana120@hotmail.com>
2022-09-10 22:01:10 -0500

    i added comments

--- turtle_crossing_game/main.py
+++ turtle_crossing_game/main.py
@@ -3,10 +3,10 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
-
+#we create the screen
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.tracer(0)
+screen.tracer(0)#we turn off the tracer
 
 game_is_on = True
 while game_is_on:
