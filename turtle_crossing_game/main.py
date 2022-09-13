d420c7b85429ec42c6ef0577e51ecac99e9c76f1
kevin santana <kevin_santana120@hotmail.com>
2022-09-12 21:46:10 -0500

    i created the turtle and added funtionalities , also i created the moving cars in the x axis

--- turtle_crossing_game/main.py
+++ turtle_crossing_game/main.py
@@ -9,6 +9,12 @@ screen.setup(width=600, height=600)
 screen.tracer(0)#we turn off the tracer
 
 game_is_on = True
+player=Player()
+cars=CarManager()
+screen.listen()#we set that our screen listen to events
+screen.onkey(player.go_up,"Up")
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    cars.create_cars()
+    cars.move_car()
