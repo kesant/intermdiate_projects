61971476139df383e90b7cb0d8c0771ac34ba487
kevin santana <kevin_santana120@hotmail.com>
2022-09-25 22:17:10 -0500

    i added the level scoreboard and the game over text when we lose the game

--- turtle_crossing_game/main.py
+++ turtle_crossing_game/main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
+
 from scoreboard import Scoreboard
 #we create the screen
 screen = Screen()
@@ -9,8 +10,11 @@ screen.setup(width=600, height=600)
 screen.tracer(0)#we turn off the tracer
 
 game_is_on = True
+
 player=Player()
 cars=CarManager()
+scoreboard=Scoreboard()
+
 screen.listen()#we set that our screen listen to events
 screen.onkey(player.go_up,"Up")
 while game_is_on:
@@ -21,9 +25,12 @@ while game_is_on:
     #detect the collision of the turtle  with the car
     for car in cars.all_cars:
         if car.distance(player)<20:
+            scoreboard.game_over()
             game_is_on=False
+
     #detect the succesful crossing
     if player.is_at_finish_lines():
         player.go_to_start()
         cars.level_up()
+        scoreboard.increase_level()
 screen.exitonclick()
