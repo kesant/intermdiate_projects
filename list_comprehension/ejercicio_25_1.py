e4e39d622eb5ee3297e32af8906f79318c51e5eb
kevin santana <kevin_santana120@hotmail.com>
2022-12-18 00:16:07 -0500

    he gregado ejercicioss usando list comprenhension

--- list_comprehension/ejercicio_25_1.py
+++ list_comprehension/ejercicio_25_1.py
@@ -1,8 +1,16 @@
 numbers=[1,1,2,3,5,8,13,21,34,55]
 squared_numbers=[ pow(number,2) for number in numbers]
-result=[number for number in numbers if number%2==0]
+import numpy
+import  pandas
+#result=[number for number in numbers if number%2==0]
 with open("file1.txt") as file:
     content_1=file.readlines()
+    content_1=[int(num.strip('\n')) for num in content_1]
     print(content_1)
+with open("file2.txt") as file:
+    content_2=file.readlines()
+    content_2=[int(num.strip('\n')) for num in content_2]
+    print(content_2)
+result=[ num2 for num in content_1 for num2 in content_2 if num2==num ]
 print(squared_numbers)
-print(result)
+print(pandas.unique(result))
