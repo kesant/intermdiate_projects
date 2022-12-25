ecea7f06baff383092d113c98d70335579f2d48d
kevin santana <kevin_santana120@hotmail.com>
2022-12-25 17:41:45 -0500

    he agregado funciones de como agregar botones y data entries

--- Tkinter_practice/main.py
+++ Tkinter_practice/main.py
@@ -1,13 +1,27 @@
 # This is a sample Python script.
-import tkinter #se importa la libreria de tkinter
+from tkinter import  *#se importa la libreria de tkinter
 
-window= tkinter.Tk()#se creo la pantalla
+window= Tk()#se creo la pantalla
 #aqui se comienza con el codigo de va en este espacio
 window.title("My first GUI program")#agrega el titulo  a la pantalla
 window.minsize(width=500,height=300)#definimos el tamano por default de la pantalla
 
 #Label
-my_label=tkinter.Label(text="aqui mi primera GUI")
+my_label=Label(text="aqui mi primera GUI",font=("Arial",24,"bold"))
+#una vez creado el label tenemos que indicar como lo vamos a mostrar en la pantalla
+my_label.pack()#muestra el label cente  rado en la pantalla
+#button
+def button_clicked():
+    print("i got clicked")
+    new_text=input.get()
+    my_label.config(text=new_text)
+button=Button(text="click Me" , command=button_clicked)
+button.pack()
+#entry component
+input= Entry(width=10)#we add a chart o add text
+input.pack()
+print(input.get())#with .get we get the information inside the chart
+
 
 
 
