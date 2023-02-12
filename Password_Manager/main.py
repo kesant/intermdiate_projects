b52200bd83081f7f0eca21085db1c3da69fd4cf5
kevin santana <kevin_santana120@hotmail.com>
2023-02-12 14:36:16 -0500

    i created the labels of the application

--- Password_Manager/main.py
+++ Password_Manager/main.py
@@ -1,6 +1,9 @@
 import tkinter
 from tkinter import  Canvas
 from tkinter import PhotoImage
+
+FONT=("Arial",15,"bold")
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -12,11 +15,24 @@ window.title("Password Manager ")#seteamos el titulo de nuestro recuadro
 window.minsize(width=500,height=300)
 window.config(padx=20,pady=20)
 #canvas
-canvas=Canvas(width=200,height=200,highlightthickness=0)#the atribute bg is for the background
+canvas=Canvas(width=200,height=200,highlightthickness=1)#the atribute bg is for the background
 #highlightthickness is a atribute for the border of the canvas
 #it is necesary make a photoimage to use the create_image
 tomato_img=PhotoImage(file="logo.png")
 canvas.create_image(100,100,image=tomato_img)#here we define the x and y position defined on the canvas
-canvas.pack()
+canvas.grid(row=0,column=1)
+
+#LABELS
+website_label=tkinter.Label(text="Website",font=FONT)
+email_user_label=tkinter.Label(text="Email/Username",font=FONT)
+password_label=tkinter.Label(text="Password",font=FONT)
+website_label.grid(row=1,column=0)
+email_user_label.grid(row=2,column=0)
+password_label.grid(row=3,column=0)
+
+#ENTRIES
+
+
+
 
 window.mainloop()
