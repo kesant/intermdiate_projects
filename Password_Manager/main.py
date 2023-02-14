8b913e0c40f45de45f3dadc7a2a5ed30f35a98e1
kevin santana <kevin_santana120@hotmail.com>
2023-02-13 22:52:06 -0500

    i added the funtionality to the add button

--- Password_Manager/main.py
+++ Password_Manager/main.py
@@ -1,19 +1,30 @@
 import tkinter
-from tkinter import  Canvas
+from tkinter import Canvas, END
 from tkinter import PhotoImage
 
-FONT=("Arial",15,"bold")
+FONT=("Arial",10,"bold")
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def  guardar_informacion():
+    website= website_entry.get()
+    email= email_user_entry.get()
+    password= password_entry.get()
+    with open("important_information.txt", mode="a") as file:
+        # changing the mode to a its that we are going to add new information
+        # to the file
+        file.write(f"\n {website}|{email}|{password}")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 window= tkinter.Tk()
 
 window.title("Password Manager ")#seteamos el titulo de nuestro recuadro
 window.minsize(width=500,height=300)
-window.config(padx=20,pady=20)
+window.config(padx=40,pady=40)
 #canvas
 canvas=Canvas(width=200,height=200,highlightthickness=1)#the atribute bg is for the background
 #highlightthickness is a atribute for the border of the canvas
@@ -23,9 +34,9 @@ canvas.create_image(100,100,image=tomato_img)#here we define the x and y positio
 canvas.grid(row=0,column=1)
 
 #LABELS
-website_label=tkinter.Label(text="Website",font=FONT)
-email_user_label=tkinter.Label(text="Email/Username",font=FONT)
-password_label=tkinter.Label(text="Password",font=FONT)
+website_label=tkinter.Label(text="Website : ",font=FONT)
+email_user_label=tkinter.Label(text="Email/Username : ",font=FONT)
+password_label=tkinter.Label(text="Password : ",font=FONT)
 website_label.grid(row=1,column=0)
 email_user_label.grid(row=2,column=0)
 password_label.grid(row=3,column=0)
@@ -33,16 +44,23 @@ password_label.grid(row=3,column=0)
 #ENTRIES
 website_entry=tkinter.Entry(width=35)
 email_user_entry=tkinter.Entry(width=35)
-password_entry=tkinter.Entry(width=21)
+password_entry=tkinter.Entry(width=19)
 #the columnspan attribute indicates how many columns it want the element go across
 website_entry.grid(row=1,column=1,columnspan=2)
+website_entry.focus()
 email_user_entry.grid(row=2,column=1,columnspan=2)
-password_entry.grid(row=3,column=1)
+email_user_entry.insert(0,"kejosant@espol.edu.ec")#insert text at a given index is like when you put a default value
+#the is an alternative as an argument for the funtion insert , if we use .inset(END) it can be a continuation of the  text
+
+password_entry.grid(row=3,column=1)#adding the pad it can be spaced between elements
+
+
+
 
 #BUTTONS
-password_button=tkinter.Button(text="Generate password" )
-add_button=tkinter.Button(text="Add",width=36)
+password_button=tkinter.Button(text="Generate Password")
 password_button.grid(row=3,column=2)
+add_button=tkinter.Button(text="Add",width=36,command=guardar_informacion)
 add_button.grid(row=4,column=1,columnspan=2)
 
 
