aad57331bd46110106a18657a3566065c7c1187f
kevin santana <kevin_santana120@hotmail.com>
2023-02-18 15:58:44 -0500

    i modifed the nato project adding exceptions

--- NATO_alphabet_project/NATO-alphabet-start/main.py
+++ NATO_alphabet_project/NATO-alphabet-start/main.py
@@ -17,15 +17,22 @@ for (index, row) in student_data_frame.iterrows():
     #Access row.student or row.score
     pass
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+#############################################################
+#here we start the coding project
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
-data=pandas.read_csv("nato_phonetic_alphabet.csv")
-data_dict={row.letter:row.code for (index,row) in data.iterrows()}
-print(data_dict)
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-usuario=input("Ingrese una palabra : ").upper()
-nato_words=[ value for letra in usuario for (key,value) in data_dict.items() if letra==key]
-print( nato_words)
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
