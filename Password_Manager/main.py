f35ac616e8f1d3c86bf698fa881f499d0b217f63
kevin santana <kevin_santana120@hotmail.com>
2023-02-19 15:50:26 -0500

    arreglos

--- Password_Manager/main.py
+++ Password_Manager/main.py
@@ -117,52 +117,52 @@ def  guardar_informacion():
         messagebox.showinfo(title="Error",message="Please don't leave any field empty!!")
 
 # ---------------------------- UI SETUP ------------------------------- #
+if __name__=="__main__":
+    window= tkinter.Tk()
+    
+    window.title("Password Manager ")#seteamos el titulo de nuestro recuadro
+    window.minsize(width=500,height=300)
+    window.config(padx=40,pady=40)
+    #canvas
+    canvas=Canvas(width=200,height=200,highlightthickness=1)#the atribute bg is for the background
+    #highlightthickness is a atribute for the border of the canvas
+    #it is necesary make a photoimage to use the create_image
+    tomato_img=PhotoImage(file="logo.png")
+    canvas.create_image(100,100,image=tomato_img)#here we define the x and y position defined on the canvas
+    canvas.grid(row=0,column=1)
 
-window= tkinter.Tk()
+    #LABELS
+    website_label=tkinter.Label(text="Website : ",font=FONT)
+    email_user_label=tkinter.Label(text="Email/Username : ",font=FONT)
+    password_label=tkinter.Label(text="Password : ",font=FONT)
+    website_label.grid(row=1,column=0)
+    email_user_label.grid(row=2,column=0)
+    password_label.grid(row=3,column=0)
 
-window.title("Password Manager ")#seteamos el titulo de nuestro recuadro
-window.minsize(width=500,height=300)
-window.config(padx=40,pady=40)
-#canvas
-canvas=Canvas(width=200,height=200,highlightthickness=1)#the atribute bg is for the background
-#highlightthickness is a atribute for the border of the canvas
-#it is necesary make a photoimage to use the create_image
-tomato_img=PhotoImage(file="logo.png")
-canvas.create_image(100,100,image=tomato_img)#here we define the x and y position defined on the canvas
-canvas.grid(row=0,column=1)
+    #ENTRIES
+    website_entry=tkinter.Entry(width=35)
+    email_user_entry=tkinter.Entry(width=35)
+    password_entry=tkinter.Entry(width=19)
+    #the columnspan attribute indicates how many columns it want the element go across
+    website_entry.grid(row=1,column=1,columnspan=2)
+    website_entry.focus()
+    email_user_entry.grid(row=2,column=1,columnspan=2)
+    email_user_entry.insert(0,"kejosant@espol.edu.ec")#insert text at a given index is like when you put a default value
+    #the is an alternative as an argument for the funtion insert , if we use .inset(END) it can be a continuation of the  text
 
-#LABELS
-website_label=tkinter.Label(text="Website : ",font=FONT)
-email_user_label=tkinter.Label(text="Email/Username : ",font=FONT)
-password_label=tkinter.Label(text="Password : ",font=FONT)
-website_label.grid(row=1,column=0)
-email_user_label.grid(row=2,column=0)
-password_label.grid(row=3,column=0)
-
-#ENTRIES
-website_entry=tkinter.Entry(width=35)
-email_user_entry=tkinter.Entry(width=35)
-password_entry=tkinter.Entry(width=19)
-#the columnspan attribute indicates how many columns it want the element go across
-website_entry.grid(row=1,column=1,columnspan=2)
-website_entry.focus()
-email_user_entry.grid(row=2,column=1,columnspan=2)
-email_user_entry.insert(0,"kejosant@espol.edu.ec")#insert text at a given index is like when you put a default value
-#the is an alternative as an argument for the funtion insert , if we use .inset(END) it can be a continuation of the  text
-
-password_entry.grid(row=3,column=1)#adding the pad it can be spaced between elements
-
-
-
-
-#BUTTONS
-search_button=tkinter.Button(text="Search",width=15,command=search_info)
-search_button.grid(row=1,column=3)
-password_button=tkinter.Button(text="Generate Password",command=generate_password)
-password_button.grid(row=3,column=3)
-add_button=tkinter.Button(text="Add",width=30,command=guardar_informacion)
-add_button.grid(row=4,column=1,columnspan=2)
+    password_entry.grid(row=3,column=1)#adding the pad it can be spaced between elements
 
 
 
-window.mainloop()
+
+    #BUTTONS
+    search_button=tkinter.Button(text="Search",width=15,command=search_info)
+    search_button.grid(row=1,column=3)
+    password_button=tkinter.Button(text="Generate Password",command=generate_password)
+    password_button.grid(row=3,column=3)
+    add_button=tkinter.Button(text="Add",width=30,command=guardar_informacion)
+    add_button.grid(row=4,column=1,columnspan=2)
+
+
+
+    window.mainloop()
