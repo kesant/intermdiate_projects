8142d331d272a2c5db980359e80ee3220e601d4b
kevin santana <kevin_santana120@hotmail.com>
2023-03-23 13:18:48 -0500

    i added the raise status for the request

--- kanye_quotes_start/main.py
+++ kanye_quotes_start/main.py
@@ -5,6 +5,7 @@ def get_quote():
 
     #Write your code here.
     response = requests.get(url="https://api.kanye.rest/")
+    response.raise_for_status()
     data=response.json()
     canvas.itemconfig(quote_text,text=data["quote"])
 
