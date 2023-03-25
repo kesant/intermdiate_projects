359184c836a9ba94243a792485bb62174146f711
kevin santana <kevin_santana120@hotmail.com>
2023-03-24 22:07:20 -0500

    i started the project rain alert

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -3,3 +3,4 @@
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+api_key=bb3e7f79f961b9e5eaa7ab9c100880f9
