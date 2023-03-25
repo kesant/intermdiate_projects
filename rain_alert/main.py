05fa45babfbdcd5c488ac063648f3b673f4221f6
kevin santana <kevin_santana120@hotmail.com>
2023-03-25 16:09:37 -0500

    obtener el clima usando una API

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -2,5 +2,24 @@
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import  requests
 
-api_key=bb3e7f79f961b9e5eaa7ab9c100880f9
+
+api_key="bb3e7f79f961b9e5eaa7ab9c100880f9"
+parameters={
+    "lat":-2.170998,
+    "lon":-79.922356,
+    "appid":api_key
+}
+response=requests.get("https://pro.openweathermap.org/data/2.5/forecast/hourly",params=parameters)
+print(response.status_code)
+response.raise_for_status()
+data=response.json()["Hourly"][:12]
+will_rain=False
+for hour_data in data:
+    condition=hour_data["weather"][0]["id"]
+    if int(condition)<700:
+        will_rain=True
+if will_rain:
+    print("Bring an umbrella")
+print(data)
