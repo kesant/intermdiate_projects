2a7b01a509eaed601efe8697f20f4f1204aaad61
kevin santana <kevin_santana120@hotmail.com>
2023-03-28 21:28:42 -0500

    i completed the first task

--- stock_trading_news_alert/stock-news-extrahard-start/main.py
+++ stock_trading_news_alert/stock-news-extrahard-start/main.py
@@ -1,9 +1,51 @@
+import requests
+import datetime
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+API_key=""
+api="https://www.alphavantage.co/query"
+parameters={
+    "function":"TIME_SERIES_DAILY_ADJUSTED",
+    "symbol":STOCK,
+    "apikey":API_key
+}
+response=requests.get(api,params=parameters)
+response.raise_for_status()
+data=response.json()['Time Series (Daily)']
 
+print(data)
+date_today=datetime.datetime.now().date()
+
+number_day_today=datetime.datetime.now().weekday()
+number_day_yesterday=number_day_today-1
+number_day_before_yesterday=number_day_yesterday-1
+if number_day_today==0:
+    yesterday = date_today - datetime.timedelta(days=3) # with the function timedelta we substract a day
+    if (number_day_today-1)==0:
+        day_before_yesterday = yesterday - datetime.timedelta(days=3)
+    else:
+        day_before_yesterday = yesterday - datetime.timedelta(days=1)
+else:
+    yesterday = date_today - datetime.timedelta(days=1)
+    if number_day_today-1==0:
+        day_before_yesterday = yesterday - datetime.timedelta(days=3)
+    else:
+        day_before_yesterday = yesterday - datetime.timedelta(days=1)
+
+
+
+closing_price_yesterday=float(data[str(yesterday)]['4. close'])
+closing_price_day_before_yesterday=float(data[str(day_before_yesterday)]['4. close'])
+porcentage_stock=round(((closing_price_yesterday-closing_price_day_before_yesterday)/closing_price_yesterday)*100,2)
+if porcentage_stock>5 and porcentage_stock<-5:
+    print("Get News")
+print(closing_price_yesterday)
+print(closing_price_day_before_yesterday)
+print(porcentage_stock)
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
