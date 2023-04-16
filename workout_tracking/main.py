58532ef7872f722267c372056534d548ae1aaa27
kevin santana <kevin_santana120@hotmail.com>
2023-04-15 22:23:11 -0500

    advance in the part two of the project

--- workout_tracking/main.py
+++ workout_tracking/main.py
@@ -1,16 +1,33 @@
 import requests
 
-APP_ID =""
-API_KEY =""
+GENDER = "MALE"
+WEIGHT_KG = 80
+HEIGHT_CM= 170
+AGE = 22
 
-headers={
-    "x-user-jwt":""
+APP_ID = MY_ID
+API_KEY = MY_KEY
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_input = input("Tell which exercise you did today?: ")
+
+header = {
+"x-app-id": "",
+"x-remote-user-id": "",
 }
 
-params={
-    "query":input("Tell me which exercise you did : ")
-
+parameters = {
+"gender": GENDER,
+"weight_kg": WEIGHT_KG,
+"height_cm": HEIGHT_CM,
+"age": AGE,
 }
+
+response = requests.post(url=exercise_endpoint, json=parameters, headers=header)
+response.raise_for_status()
+result = response.json()
+print(result)
 
 
 
