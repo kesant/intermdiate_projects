8151f7efd0b0aabb7e1fb3ad1d1767b8af83fd99
kevin santana <kevin_santana120@hotmail.com>
2023-04-16 12:58:32 -0500

    i updated the flight deal finder project files

--- flight_deal_finder/main.py
+++ flight_deal_finder/main.py
@@ -1,4 +1,1 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
