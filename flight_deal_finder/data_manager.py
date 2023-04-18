4cb29bd68ebd2c3994433be2955c54edfac4c360
kevin santana <kevin_santana120@hotmail.com>
2023-04-17 23:05:35 -0500

    i made some advances in the prooject

--- flight_deal_finder/data_manager.py
+++ flight_deal_finder/data_manager.py
@@ -1,3 +1,25 @@
+import requests
+import os
+from pprint import pprint
+
+# list={"Paris":"PAR",
+#     "Berlin":BER,
+#     "Tokyo":,
+#     "Sydney":,
+#     "Istanbul":,
+#     "Kuala Lumpur":,
+#     "New York":,
+#     "San Francisco":,
+#     "Cape Town":""
+#
+#       }
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
     pass
+    def get_data(self):
+        API_GOOGLE_SHEET = "https://api.sheety.co/99590fd56f514951e3e0791088f7aa9f/flightDeals/prices"
+
+        response = requests.get(API_GOOGLE_SHEET)
+        data = response.json()['prices']
+        return data
--- flight_deal_finder/main.py
+++ flight_deal_finder/main.py
@@ -1,1 +1,23 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+import requests
+import os
+from pprint import pprint
+
+
+#EDIT THE ROWS
+API_GOOGLE_SHEET="https://api.sheety.co/99590fd56f514951e3e0791088f7aa9f/flightDeals/prices"
+
+response=requests.get(API_GOOGLE_SHEET)
+data=response.json()
+pprint(data)
+
+
+
+
+
+
+
+
+
+
+
